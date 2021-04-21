ade355f816a368be0aa7b20123d3b6a88f3b575e
gitReadyGitSet <76755591+gitreadygitset@users.noreply.github.com>
2021-04-21 10:49:13 -0400

    Refactor tests to use FactoryBot

--- spec/models/coffee_shop_spec.rb
+++ spec/models/coffee_shop_spec.rb
@@ -1,8 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe CoffeeShop, type: :model do
-  let!(:test_shop_1){ CoffeeShop.create(name: "Starbucks", address: "123 Fake St.", city: "Boston", state: "MA", zip: "02169", description: "Best lattes in town would recommend to a friend.") }
-  let!(:test_shop_2){ CoffeeShop.new(address: "123 Fake St.", city: "Boston", state: "MA", zip: "02169") }
+  test_shop_1 = FactoryBot.create(:coffee_shop)
+  test_shop_2 = FactoryBot.build(:coffee_shop, name: "" )
+  
 
   describe "#create" do
       context "Coffee Shop is created correctly with required fields and parameters" do
@@ -11,22 +12,21 @@ RSpec.describe CoffeeShop, type: :model do
           expect(test_shop_1.address).to eq("123 Fake St.")
           expect(test_shop_1.city).to eq("Boston")
           expect(test_shop_1.state).to eq("MA")
-          expect(test_shop_1.zip).to eq("02169")
+          expect(test_shop_1.zip).to eq("02135")
           expect(test_shop_1.description).to eq("Best lattes in town would recommend to a friend.")
         end
       end
 
       context "Coffee Shop is not created without required fields and parameters" do
-        let!(:test_shop_4){ CoffeeShop.new(name: "Test Shop", address: "123 Fake St.", city: "Killington", state: "VT", zip: "02") }
-        let!(:test_shop_5){ CoffeeShop.new(name: "Donut Shop", address: "123 Fake St.", city: "Killington", state: "VT", zip: "0214563") }
-        let!(:test_shop_6){ CoffeeShop.new(name: "Tattes", address: "123 Fake St.", city: "Killington", state: "VT", zip: "02145", description: 'This is short') }
+        test_shop_4 = FactoryBot.build(:coffee_shop, zip: "02" )
+        test_shop_5 = FactoryBot.build(:coffee_shop, zip: "021456")
+        test_shop_6 = FactoryBot.build(:coffee_shop, description: "This is short" )
 
         it "does not create a new coffee shop if a required attribute is missing" do
           expect(test_shop_2.save).to eq(false)
         end
 
         it "does not create a new coffee shop if the zip code is not 5 digits" do
-
           expect(test_shop_4.save).to eq(false)
           expect(test_shop_5.save).to eq(false)
         end
--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -6,5 +6,13 @@ FactoryBot.define do
     password { 'password' }
     password_confirmation { 'password' }
   end
-
+  
+  factory :coffee_shop do 
+    name { "Starbucks" }
+    address { "123 Fake St." }
+    city { "Boston" }
+    state { "MA" }
+    zip { "02135" }
+    description { "Best lattes in town would recommend to a friend." }
+  end
 end
