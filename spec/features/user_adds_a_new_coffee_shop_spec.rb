3199a7058021cf170027c3e8e01d1e30fdcd1e4d
gitReadyGitSet <76755591+gitreadygitset@users.noreply.github.com>
2021-04-23 15:21:58 -0400

    Add feature testing to new coffee shop form. Minor styling changes.
    Co-authored-by: Osnyder12 <Osnyder12@users.noreply.github.com>
    Co-authored-by: tjswierzewski <tjswierzewski@users.noreply.github.com>
    Co-authored-by: nicolesmith31 <nicolesmith31@users.noreply.github.com>

---
+++ spec/features/user_adds_a_new_coffee_shop_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+feature "User adds a new coffee shop" do
+  scenario "User visits the index page and clicks on a link to go to the new coffee shop form" do
+    visit coffee_shops_path
+    click_link("Click here to add it!")
+    expect(page).to have_content("Add Your Favorite Coffee Shop")
+  end
+
+  scenario "User correctly fills out the form and submits a coffee shop" do
+
+    visit new_coffee_shop_path
+    fill_in 'Name', with: "Fuel"
+    fill_in 'Address', with: "152 Chestnut Hill Ave."
+    fill_in 'City', with: "Brighton"
+    fill_in 'State', with: "MA"
+    fill_in 'Zip', with: "02135"
+    click_button 'Add Coffee Shop'
+
+    expect(CoffeeShop.count).to eq(1)
+
+    expect(page).to have_current_path(coffee_shop_path(CoffeeShop.first))
+    expect(page).to have_content('Coffee Shop Added')
+  end
+
+  scenario "User incorrectly fills out the form and submits a coffee shop" do
+
+    visit new_coffee_shop_path
+    fill_in 'Description', with: 'the'
+    click_button 'Add Coffee Shop'
+
+    expect(CoffeeShop.count).to eq(0)
+    expect(page).to have_content("Name can't be blank")
+    expect(page).to have_content("Address can't be blank")
+    expect(page).to have_content("City can't be blank")
+    expect(page).to have_content("State can't be blank")
+    expect(page).to have_content("Zip can't be blank")
+    expect(page).to have_content("Description is too short (minimum is 20 characters)")
+  end
+end
