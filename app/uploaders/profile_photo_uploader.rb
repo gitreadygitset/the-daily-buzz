8da825dcefa57befda15666673e02ab4f8d53f67
gitReadyGitSet <76755591+gitreadygitset@users.noreply.github.com>
2021-04-26 14:33:11 -0400

    Co-authored-by: nicolesmith31 <nicolesmith31@users.noreply.github.com>

--- app/uploaders/profile_photo_uploader.rb
+++ app/uploaders/profile_photo_uploader.rb
@@ -15,7 +15,7 @@ class ProfilePhotoUploader < CarrierWave::Uploader::Base
   # This is a sensible default for uploaders that are meant to be mounted:
 
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "public/testuploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
