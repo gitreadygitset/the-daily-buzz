5b1a1496818db8ef60148fb53d2555ab1e5d2b23
Tim Swierzewski <TJswierzewski@gmail.com>
2021-04-26 14:39:13 -0400

    Add belongs to for reivews and users

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,8 @@
 class Review < ApplicationRecord
-  
-  validates :rating, numericality: { less_than_or_equal_to: 5 } 
-  validates :comment, length: { maximum: 500 }, allow_blank: true
-  
+  belongs_to :user
   belongs_to :coffee_shop
+
+  validates :rating, numericality: { less_than_or_equal_to: 5 }
+  validates :comment, length: { maximum: 500 }, allow_blank: true
 end
+
