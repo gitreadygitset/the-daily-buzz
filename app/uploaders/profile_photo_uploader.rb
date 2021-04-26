f4b996cdd3141190b7a05c93b3f94acd7c39ed02
gitReadyGitSet <76755591+gitreadygitset@users.noreply.github.com>
2021-04-26 13:53:38 -0400

    Configure S3 storage and heroku to accept profile pics

--- app/uploaders/profile_photo_uploader.rb
+++ app/uploaders/profile_photo_uploader.rb
@@ -4,11 +4,16 @@ class ProfilePhotoUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  if Rails.env.test?
+    storage :file
+  else
+    storage :fog
+  end
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
+
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
