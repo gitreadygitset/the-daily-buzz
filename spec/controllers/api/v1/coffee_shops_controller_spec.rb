22d562d2da8f0751aa43cf5d0c6d49f34ec73f7a
Tim Swierzewski <TJswierzewski@gmail.com>
2021-04-27 14:26:42 -0400

    Add current_user to coffeeshop show page API

--- spec/controllers/api/v1/coffee_shops_controller_spec.rb
+++ spec/controllers/api/v1/coffee_shops_controller_spec.rb
@@ -11,17 +11,19 @@ RSpec.describe Api::V1::CoffeeShopsController, type: :controller do
 
   describe "GET#show" do
     it "Should return a coffee shop and its reviews" do
-
+      sign_in user
       get :show, params: {id: test_shop.id}
       returned_json = JSON.parse(response.body)
-      
+
       expect(returned_json["coffee_shop"]["address"]).to eq("123 Fake St.")
       expect(returned_json["coffee_shop"]["city"]).to eq("Boston")
       expect(returned_json["coffee_shop"]["state"]).to eq("MA")
       expect(returned_json["coffee_shop"]["zip"]).to eq("02135")
       expect(returned_json["coffee_shop"]["description"]).to eq("Best lattes in town would recommend to a friend.")
       expect(returned_json["coffee_shop"]["image_url"]).to eq(nil)
-      expect(returned_json["coffee_shop"]["reviews"].length).to eq(4) 
+      expect(returned_json["coffee_shop"]["reviews"].length).to eq(4)
+      expect(returned_json["coffee_shop"]["current_user"]["username"]).to eq("GuestMember123")
+
     end
   end
 end
