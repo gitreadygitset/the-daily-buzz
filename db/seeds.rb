90ea73509ef67726386ab39c62996db417b0014d
Nicole <nicolesmith5724@icloud.com>
2021-04-27 10:16:38 -0400

    Work on styling, formatting, and seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-cumberland1 = CoffeeShop.find_or_create_by(name: "Cumberland Farms", address: "the corner", city: "Boston", state: "MA", zip: "02135", description: "It's a gas station extraordinaire with some delicious coffee", image_url: "https://www.sun-sentinel.com/resizer/272RrIvd4s6hAu3o_wxfph9t9H8=/415x213/top/arc-anglerfish-arc2-prod-tronc.s3.amazonaws.com/public/I6ENXUCQNZDSXJ3YIMEEVB63VE.jpg")
-mirror1 = CoffeeShop.find_or_create_by(name: "Cafe Mirror", address: "the other corner", city: "Brighton", state: "MA", zip: "02135")
+cumberland1 = CoffeeShop.find_or_create_by!(name: "Cumberland Farms", address: "the corner", city: "Boston", state: "MA", zip: "02135", description: "It's a gas station extraordinaire with some delicious coffee", image_url: "https://www.sun-sentinel.com/resizer/272RrIvd4s6hAu3o_wxfph9t9H8=/415x213/top/arc-anglerfish-arc2-prod-tronc.s3.amazonaws.com/public/I6ENXUCQNZDSXJ3YIMEEVB63VE.jpg")
+mirror1 = CoffeeShop.find_or_create_by!(name: "Cafe Mirror", address: "the other corner", city: "Brighton", state: "MA", zip: "02135")
 
-Review.find_or_create_by(rating: 3, comment: "It's fine but nothing to write home about.", coffee_shop: cumberland1)
-Review.find_or_create_by(rating: 4, comment: "Really good for a gas station.", coffee_shop: cumberland1)
+user1 = User.new
+user1.email = "un1corn@rainbow.com"
+user1.username = "un1corn"
+user1.password = "seedymcseedsalot"
+user1.save!
+
+Review.find_or_create_by!(rating: 3, comment: "It's fine but nothing to write home about.", coffee_shop: cumberland1, user: user1)
+Review.find_or_create_by!(rating: 4, comment: "Really good for a gas station.", coffee_shop: cumberland1, user: user1)
